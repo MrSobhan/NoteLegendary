77f68edd990879e66eb3827b352e42013f37837e
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-02-22 21:49:22 +0330

    Change Ui Singup Page with Tailwind

--- singup.php
+++ singup.php
@@ -44,8 +44,8 @@ include_once("./includes/header.php");
 
 
 
-<body>
-    <div class="container p-5 shadow">
+<body class="h-full">
+    <!-- <div class="container p-5 shadow">
         <form class="form" method="post">
             <a href=<?= href('home.php') ?> style="text-decoration: none;cursor:pointer;">
                 <h1 class="fs-3 text-center mb-3 text-dark">Legendary Notes <i class="bi bi-pencil-square"></i></h1>
@@ -73,6 +73,53 @@ include_once("./includes/header.php");
                 </a>
             </center>
         </form>
+    </div> -->
+
+    <div class="flex min-h-full w-[300px] flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="#" method="POST">
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">نام:</label>
+                    <div class="mt-2">
+                        <input id="text" name="num" type="text" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2">
+                    </div>
+                </div>
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">ايميل:</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2">
+                    </div>
+                </div>
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">تلفن:</label>
+                    <div class="mt-2">
+                        <input id="phone" name="phone" type="number" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2">
+                    </div>
+                </div>
+
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">رمز ورود:</label>
+                    </div>
+                    <div class="mt-2">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2">
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" name="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ثبت نام</button>
+                </div>
+            </form>
+
+            <p class="mt-10 text-center text-sm text-gray-500">
+                مثل اینکه اکانت داری برو به 
+                <a href=<?= href('login.php') ?> class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">ورود</a>
+            </p>
+        </div>
     </div>
 
     <script src="./js/main.js"></script>
