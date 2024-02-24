b2a9a3f72b2da449acb8de7f1bcdad3849875ee1
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-02-24 20:50:45 +0330

    Edit Part 2

--- home.php
+++ home.php
@@ -143,7 +143,7 @@ include_once("./includes/header.php");
                 </dl>
               </div>
             </div>
-            <img src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
+            <img src="./images/features/Add notes-bro.png" alt="Product screenshot" class="w-auto">
           </div>
         </div>
       </div>
