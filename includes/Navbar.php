212bb2850042a2edb42536af344fd0b2b7a82914
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-02-24 20:35:01 +0330

    Add Logo NavBar

--- includes/Navbar.php
+++ includes/Navbar.php
@@ -88,7 +88,7 @@
 
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex flex-shrink-0 items-center me-6">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+        <a href="./home.php"><img class="h-8 w-auto" src="../images/NoteLegendary.png" alt="Your Company"></a>
         </div>
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
