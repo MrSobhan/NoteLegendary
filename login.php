9cd2916846e6fff4456b15609130f72a8c05fb63
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-02-24 20:30:52 +0330

    Add Logo Page login And singup

--- login.php
+++ login.php
@@ -61,8 +61,8 @@ include_once("./includes/header.php");
 
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">به حساب خود وارد شوید.</h2>
+            <a href="./home.php"><img class="mx-auto h-40 w-auto" src="./images/NoteLegendary.png" alt="Your Company"></a>
+            <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">به حساب خود وارد شوید.</h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
