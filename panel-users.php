744226b8decbaa41eef8d94e1ef521ba67d16c95
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-02-25 22:32:58 +0330

    End Ui Page Panel User

--- panel-users.php
+++ panel-users.php
@@ -111,7 +111,12 @@ include_once("./includes/header.php");
 
   <?php include_once('./includes/Navbar.php'); ?>
 
-  <section>
+  <div class="absolute inset-x-0 top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+    <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+  </div>
+
+  <section class="mx-auto max-w-7xl px-6 lg:px-8">
+    <h1 class="moraba  text-2xl text-indigo-800 my-10"><?= get_session('uname'); ?> به پنل کاربری خوش آمدید <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Waving%20Hand%20Light%20Skin%20Tone.png" class="w-auto h-8 inline" alt=""></h1>
     <div class="container mt-5">
       <form action="" method="post">
         <?php
@@ -121,52 +126,60 @@ include_once("./includes/header.php");
         ?>
             <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label">نام شما:</label>
-              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="سبحان" value="<?= $user['username'] ?>" name="name">
+              <input type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2" id="exampleFormControlInput1" placeholder="سبحان" value="<?= $user['username'] ?>" name="name">
             </div>
             <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label">ايميل شما:</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Sobhan@gmail.com" value="<?= $user['email'] ?>" name="email">
+              <input type="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2" id="exampleFormControlInput1" placeholder="Sobhan@gmail.com" value="<?= $user['email'] ?>" name="email">
             </div>
             <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label">شماره تلفن شما:</label>
-              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="09128422739" value="<?= $user['phonenumber'] ?>" name="phone">
+              <input type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2" id="exampleFormControlInput1" placeholder="09128422739" value="<?= $user['phonenumber'] ?>" name="phone">
             </div>
             <div class="mb-3">
               <label for="exampleFormControlTextarea1" class="form-label">نوع کاربری</label>
-              <input type="text" class="form-control" id="exampleFormControlInput1" value="<?= $user['type'] == 'user' ? 'کاربر عادی' : 'کاربر ادمین' ?>" name="type" disabled>
+              <input type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2" id="exampleFormControlInput1" value="<?= $user['type'] == 'user' ? 'کاربر عادی' : 'کاربر ادمین' ?>" name="type" disabled>
             </div>
             <div class="mb-3">
               <label for="exampleFormControlTextarea1" class="form-label">تاریخ عضویت</label>
-              <input type="text" class="form-control" id="exampleFormControlInput1" value="<?= $user['createdAt']  ?>" name="dateCreate" disabled>
+              <input type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2" id="exampleFormControlInput1" value="<?= $user['createdAt']  ?>" name="dateCreate" disabled>
             </div>
 
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <a href="<?= href('home.php'); ?>"><button class="btn btn-lg btn-outline-danger" type="button"><i class="bi bi-x-circle"></i> لغو</button></a>
-                <button class="btn btn-lg btn-outline-primary" name="submitUser"><i class="bi bi-pen"></i> ويراش</button>
-              </div>
-          <?php
+            <div class="mt-4">
+              <button class="flex w-[250px] mx-auto justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" name="submitUser"><i class="bi bi-pen"></i> ويراش</button>
+            </div>
+        <?php
           }
         }
-          ?>
+        ?>
       </form>
     </div>
   </section>
 
-  <section>
+  <section class="mx-auto max-w-7xl px-6 lg:px-8 mb-20">
     <?php
     if ($TypeUser != 'user') {
     ?>
-      <h1 class="text-center text-primary mt-5">:) Hello Admin</h1>
-      <div class="container-fluid mt-5">
-        <table class="table table-responsiv border table-striped">
-          <thead>
+    <h1 class="moraba  text-2xl text-indigo-800 my-10">به پنل مدیریت خوش آمدید <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Waving%20Hand%20Light%20Skin%20Tone.png" class="w-auto h-8 inline" alt=""></h1>
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th>Id</th>
-              <th>ايميل</th>
-              <th>گفت و گو</th>
-              <th>زمان انتشار</th>
-              <th>وضعيت</th>
+              <th scope="col" class="px-6 py-3">
+                Id
+              </th>
+              <th scope="col" class="px-6 py-3">
+                ايميل
+              </th>
+              <th scope="col" class="px-6 py-3">
+                گفت و گو
+              </th>
+              <th scope="col" class="px-6 py-3">
+                زمان انتشار
+              </th>
+              <th scope="col" class="px-6 py-3">
+                وضعيت
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -174,14 +187,14 @@ include_once("./includes/header.php");
             while ($chat = $allChats->fetch_assoc()) {
               if ($chat['status'] == 0) {
             ?>
-                <tr>
-                  <td><?= $chat['id'] ?></td>
-                  <td><?= $chat['email'] ?></td>
+                <tr class="bg-white border-b hover:bg-gray-50">
+                  <td class="px-6 py-4"><?= $chat['id'] ?></td>
+                  <td class="px-6 py-4"><?= $chat['email'] ?></td>
                   <td><?= $chat['chat'] ?></td>
-                  <td><?= $chat['createdAt'] ?></td>
-                  <td>
-                    <a href="<?= href('panel-users.php?id=' . $ID . '&deletechats=' . $chat['id']) ?>"><button class="btn btn-outline-danger fs-5 "><i class="bi bi-x-circle"></i></button></a>
-                    <a href="<?= href('panel-users.php?id=' . $ID . '&changestatus=' . $chat['id']) ?>"><button class="btn btn-outline-success  fs-5 "><i class="bi bi-bookmark-check"></i></button></a>
+                  <td class="px-6 py-4"><?= $chat['createdAt'] ?></td>
+                  <td class="px-6 py-4">
+                    <a href="<?= href('panel-users.php?id=' . $ID . '&deletechats=' . $chat['id']) ?>"><button class="text-indigo-800 text-xl"><i class="bi bi-x-circle"></i></button></a>
+                    <a href="<?= href('panel-users.php?id=' . $ID . '&changestatus=' . $chat['id']) ?>"><button class="text-indigo-800 text-xl"><i class="bi bi-bookmark-check"></i></button></a>
                   </td>
                 </tr>
 
@@ -195,6 +208,7 @@ include_once("./includes/header.php");
     <?php
     }
     ?>
+
   </section>
 
   <?php include_once('./includes/footer.php'); ?>
