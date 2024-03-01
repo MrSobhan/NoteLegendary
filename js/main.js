6f7b68472a5574fd9a1609eb8662166cdee18b23
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-03-01 18:13:58 +0330

    Edit Footer Svg And Clear Code Notes-add Page

--- js/main.js
+++ js/main.js
@@ -8,58 +8,58 @@ _qsA = (qsA) => $.querySelectorAll(qsA);
 // })
 
 // $.addEventListener("load", () => {
-  if (_qs("#editor")) {
-    ClassicEditor.create(_qs("#editor")).catch((error) => {
-      console.error(error);
-    });
-  }
+if (_qs("#editor")) {
+  ClassicEditor.create(_qs("#editor")).catch((error) => {
+    console.error(error);
+  });
+}
 
-  _qs(".nav-open").addEventListener("click", () => {
-    let status = _qs(".mobile-menu").getAttribute("open");
+_qs(".nav-open").addEventListener("click", () => {
+  let status = _qs(".mobile-menu").getAttribute("open");
+
+  if (status == "false") {
+    _qs(".mobile-menu").classList.remove("hidden");
+    _qs(".menu").classList.add("hidden");
+    _qs(".exit").classList.remove("hidden");
+    _qs(".mobile-menu").setAttribute("open", "true");
+  } else {
+    _qs(".mobile-menu").classList.add("hidden");
+    _qs(".menu").classList.remove("hidden");
+    _qs(".exit").classList.add("hidden");
+    _qs(".mobile-menu").setAttribute("open", "false");
+  }
+});
+
+if (_qs("#user-menu-button")) {
+  _qs("#user-menu-button").addEventListener("click", () => {
+    let status = _qs(".dropdown-profile").getAttribute("open");
 
     if (status == "false") {
-      _qs(".mobile-menu").classList.remove("hidden");
-      _qs(".menu").classList.add("hidden");
-      _qs(".exit").classList.remove("hidden");
-      _qs(".mobile-menu").setAttribute("open", "true");
+      _qs(".dropdown-profile").classList.remove("hidden");
+      _qs(".dropdown-profile").setAttribute("open", "true");
     } else {
-      _qs(".mobile-menu").classList.add("hidden");
-      _qs(".menu").classList.remove("hidden");
-      _qs(".exit").classList.add("hidden");
-      _qs(".mobile-menu").setAttribute("open", "false");
+      _qs(".dropdown-profile").classList.add("hidden");
+      _qs(".dropdown-profile").setAttribute("open", "false");
     }
   });
+}
 
-  if (_qs("#user-menu-button")) {
-    _qs("#user-menu-button").addEventListener("click", () => {
-      let status = _qs(".dropdown-profile").getAttribute("open");
-
-      if (status == "false") {
-        _qs(".dropdown-profile").classList.remove("hidden");
-        _qs(".dropdown-profile").setAttribute("open", "true");
-      } else {
-        _qs(".dropdown-profile").classList.add("hidden");
-        _qs(".dropdown-profile").setAttribute("open", "false");
-      }
-    });
+$.addEventListener("scroll", () => {
+  if ($.documentElement.scrollTop <= 8) {
+    _id("navbar").style.position = "relative";
+    _id("navbar").classList.remove("bg-white");
+    _id("navbar").classList.remove("shadow");
+    _qs(".btnTop").classList.add("hidden");
+  } else {
+    _id("navbar").style.position = "fixed";
+    _id("navbar").style.top = "0px";
+    _id("navbar").classList.add("bg-white");
+    _id("navbar").classList.add("shadow");
+    _qs(".btnTop").classList.remove("hidden");
   }
+});
 
-  $.addEventListener("scroll", () => {
-    if ($.documentElement.scrollTop <= 8) {
-      _id("navbar").style.position = "relative";
-      _id("navbar").classList.remove("bg-white");
-      _id("navbar").classList.remove("shadow");
-      _qs(".btnTop").classList.add("hidden");
-    } else {
-      _id("navbar").style.position = "fixed";
-      _id("navbar").style.top = "0px";
-      _id("navbar").classList.add("bg-white");
-      _id("navbar").classList.add("shadow");
-      _qs(".btnTop").classList.remove("hidden");
-    }
-  });
+// _qs(".btnTop").addEventListener("click", ($.documentElement.scrollTop = 0));
 
-  // _qs(".btnTop").addEventListener("click", ($.documentElement.scrollTop = 0));
-
-  console.log("Hello World :))");
+console.log("Hello World :))");
 // });
--- notes-add.php
+++ notes-add.php
@@ -19,47 +19,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($text  && $title) {
 
             $ID = uniqid();
-            
+
             $queryAddNotes = "INSERT INTO `note`(`id`,`uid`, `title`, `text` ) VALUES ('$ID','$id_user','$title','$text')";
 
             $result = mysqli_query($conn, $queryAddNotes);
             if ($result) {
-                Locatoin('notes.php?id='. get_session('id'));
+                Locatoin('notes.php?id=' . get_session('id'));
             }
-        }else{
+        } else {
             alertErrorInput();
         }
     }
 
     if (isset($_POST['noteBack'])) {
-        Locatoin('notes.php?id='. get_session('id'));
+        Locatoin('notes.php?id=' . get_session('id'));
     }
 }
 $page_type = 'notes';
 include_once("./includes/header.php");
+// include_once('./includes/Navbar.php');
+
 ?>
 
 
 <body>
-    <h1 class="text-primary text-center mt-4"><i class="bi bi-chat-left-text"></i> ايجاد يادداشت</h1>
-    <div class="container mt-5">
-        <form action="" method="post">
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">موضوع متن:</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="موضوع" name="title">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">متن:</label>
-                <textarea id="editor" name="text" placeholder="...روز اول"></textarea>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <button class="btn btn-lg btn-outline-danger" name="noteBack"><i class="bi bi-x-circle"></i> لغو</button>
-                    <button class="btn btn-lg btn-outline-success" name="sub"><i class="bi bi-bookmark-plus-fill"></i> ذخيره</button>
-                </div>
-            </div>
-        </form>
+    <div class="absolute inset-x-0 top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
+    <main class="mx-auto max-w-7xl px-6 lg:px-8">
+        <h1 class="text-3xl moraba my-10 mx-auto"><img src="https://github.com/Tarikul-Islam-Anik/tarikul-islam-anik/raw/main/assets/images/Hourglass%20Done.png" class="h-8 w-8 inline" alt=""> ايجاد يادداشت</h1>
+        <div class="container mt-5">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="text-indigo-800 mb-4">عنوان یادداشت:</label>
+                    <input type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2" placeholder="موضوع" name="title">
+                </div>
+                <div class="mb-3">
+                    <!-- <label for="exampleFormControlTextarea1" class="text-indigo-800 mb-4">متن یادداشت:</label> -->
+                    <textarea id="editor" name="text" class="text-indigo-800 text-end mt-5" placeholder="...روز اول"></textarea>
+                </div>
+
+
+                <button  type="submit" name="sub" class="bookmarkBtn shadow-lg bg-gray-50">
+                    <span class="IconContainer">
+                        <svg viewBox="0 0 384 512" height="0.9em" class="icon">
+                            <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"></path>
+                        </svg>
+                    </span>
+                    <p class="text">ذخیره</p>
+                </button>
+
+                <!-- <button class="btn btn-lg btn-outline-danger" name="noteBack"><i class="bi bi-x-circle"></i> لغو</button> -->
+                <!-- <button type="submit" name="submit" class="flex w-[200px] justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ذخیره</button> -->
+            </form>
+        </div>
+    </main>
 
     <?php include_once('./includes/footer.php'); ?>
     <script src="./js/main.js"></script>
