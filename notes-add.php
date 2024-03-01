b3eb64c5d56c23ba9922991e3616bc5e98c01aa6
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-03-01 18:15:14 +0330

    Add Svg footer

--- notes-add.php
+++ notes-add.php
@@ -42,12 +42,26 @@ include_once("./includes/header.php");
 ?>
 
 
-<body>
+<body class="overflow-x-hidden">
     <div class="absolute inset-x-0 top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
     <main class="mx-auto max-w-7xl px-6 lg:px-8">
-        <h1 class="text-3xl moraba my-10 mx-auto"><img src="https://github.com/Tarikul-Islam-Anik/tarikul-islam-anik/raw/main/assets/images/Hourglass%20Done.png" class="h-8 w-8 inline" alt=""> ايجاد يادداشت</h1>
+        <div class="flex items-center justify-between my-10">
+            <h1 class="text-3xl moraba"><img src="https://github.com/Tarikul-Islam-Anik/tarikul-islam-anik/raw/main/assets/images/Hourglass%20Done.png" class="h-8 w-8 inline" alt=""> ايجاد يادداشت</h1>
+            <form action="" method="post">
+                <button name="noteBack" class="relative shadow-lg border-2 rounded-lg border-indigo-800 group w-9 h-9 duration-500 overflow-hidden" type="submit">
+                    <p class="font-Manrope mt-0.5 text-3xl h-full w-full flex items-center justify-center text-black duration-500 relative z-10 group-hover:scale-0">
+                        ×
+                    </p>
+                    <span class="absolute w-full h-full bg-indigo-800 rotate-45 group-hover:top-7 duration-500 top-12 left-0"></span>
+                    <span class="absolute w-full h-full bg-indigo-800 rotate-45 top-0 group-hover:left-7 duration-500 left-12"></span>
+                    <span class="absolute w-full h-full bg-indigo-800 rotate-45 top-0 group-hover:right-7 duration-500 right-12"></span>
+                    <span class="absolute w-full h-full bg-indigo-800 rotate-45 group-hover:bottom-7 duration-500 bottom-12 right-0"></span>
+                </button>
+            </form>
+        </div>
+
         <div class="container mt-5">
             <form action="" method="post">
                 <div class="mb-3">
@@ -60,7 +74,7 @@ include_once("./includes/header.php");
                 </div>
 
 
-                <button  type="submit" name="sub" class="bookmarkBtn shadow-lg bg-gray-50">
+                <button type="submit" name="sub" class="bookmarkBtn shadow-lg bg-gray-50 mb-4">
                     <span class="IconContainer">
                         <svg viewBox="0 0 384 512" height="0.9em" class="icon">
                             <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"></path>
@@ -68,9 +82,6 @@ include_once("./includes/header.php");
                     </span>
                     <p class="text">ذخیره</p>
                 </button>
-
-                <!-- <button class="btn btn-lg btn-outline-danger" name="noteBack"><i class="bi bi-x-circle"></i> لغو</button> -->
-                <!-- <button type="submit" name="submit" class="flex w-[200px] justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ذخیره</button> -->
             </form>
         </div>
     </main>
